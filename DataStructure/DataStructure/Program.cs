786fa5547d272e516ad300b9f04547da001cb672
OsamaAlzaghal <osama.alzaghal@gmail.com>
2022-04-27 02:47:33 +0300

    Solved challenge 15

--- DataStructure/DataStructure/Program.cs
+++ DataStructure/DataStructure/Program.cs
@@ -5,8 +5,10 @@ using DataStructure.DSA.LinkedList;
 using DataStructure.DSA.QueueUsingStacks;
 using DataStructure.DSA.Stack_Queue;
 using DataStructure.DSA.AnimalsShelterQueue;
-using System;
+using DataStructure.DSA.Tree;
 using DataStructure.DSA.Stack_Queue_Brackets;
+using Node = DataStructure.DSA.Tree.Node;
+using System;
 
 namespace DataStructure
 {
@@ -122,7 +124,23 @@ namespace DataStructure
             //Console.WriteLine(animal2.Name);
             //Console.WriteLine(animal3.Name);
 
-            Console.WriteLine(StackQueueBrackets.ValidateBrackets("{(})"));
+            //Console.WriteLine(StackQueueBrackets.ValidateBrackets("{(})"));
+
+            BinaryTree binaryTree = new BinaryTree(new Node(10));
+            binaryTree.Root.Left = new Node(8);
+            binaryTree.Root.Right = new Node(15);
+            binaryTree.Root.Left.Left = new Node(5);
+            binaryTree.Root.Right.Right = new Node(20); 
+            Console.WriteLine(binaryTree.ToString(binaryTree.PreOrder(binaryTree.Root)));
+            Console.WriteLine(binaryTree.ToString(binaryTree.PostOrder(binaryTree.Root)));
+            Console.WriteLine(binaryTree.ToString(binaryTree.InOrder(binaryTree.Root)));
+            BinarySearchTree binarySearchTree = new BinarySearchTree(new Node(10));
+            binarySearchTree.Add(8);
+            binarySearchTree.Add(15);
+            binarySearchTree.Add(5);
+            binarySearchTree.Add(20);
+            Console.WriteLine(binarySearchTree.Contains(binaryTree.Root, 7));
+            Console.WriteLine(binarySearchTree.ToString(binarySearchTree.InOrder(binarySearchTree.Root)));
         }
     }
 }
